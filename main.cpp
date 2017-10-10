d14ef7c5754c58937465f0c0315ac021d7818920
ricardo1447 <rflores42@mail.csuchico.edu>
2017-10-10 13:50:56 -0700

    Spaces out evenly

--- main.cpp
+++ main.cpp
@@ -12,8 +12,19 @@ for(int row=1; row < 13; row++)
 {
     for(int col=1; col < 13; col++)
     {
-        cout << row * col << " "; 
-        
+        if( (row * col) < 10)
+        {
+            cout << row * col << "   |";
+        }
+        else if( (row * col) < 100)
+        {
+            cout << row * col << "  |";
+        }
+        else
+        {
+            cout << row * col << " |";
+        }
+
     }
 cout << endl;
 }
